abd0736b98b3c8ff16fd830623ba776df85f3044
ZohaibA99 <118318498+ZohaibA99@users.noreply.github.com>
2023-11-08 16:02:17 -0500

    started project implementation and front-end

---
+++ client/src/App.js
@@ -0,0 +1,22 @@
+import {BrowserRouter, Routes, Route} from 'react-router-dom';
+import Home from './pages/Home';
+import About from './pages/About';
+import SignIn from './pages/SignIn';
+import SignUp from './pages/SignUp';
+import Profile from './pages/Profile';
+import Header from './components/Header'
+
+export default function App() {
+  return (
+    <BrowserRouter>
+        <Header></Header>
+        <Routes>
+          <Route path='/' element={<Home />}></Route>
+          <Route path='/about' element={<About />}></Route>
+          <Route path='/sign-in' element={<SignIn />}></Route>
+          <Route path='/sign-up' element={<SignUp />}></Route>
+          <Route path='/profile' element={<Profile />}></Route>
+        </Routes>
+    </BrowserRouter>
+  )
+}
