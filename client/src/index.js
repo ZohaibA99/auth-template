4d511b81185a4749df492e2e1faa013fa02a5fd0
ZohaibA99 <118318498+ZohaibA99@users.noreply.github.com>
2023-11-14 14:10:49 -0500

    added redux persist

--- client/src/index.js
+++ client/src/index.js
@@ -2,13 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App.js';
-import {store} from './redux/store.js';
-import {Provider} from 'react-redux'
+import {store, persistor} from './redux/store.js';
+import {Provider} from 'react-redux';
+import {PersistGate} from 'redux-persist/integration/react';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <Provider store={store}>
-    <App />
+    <PersistGate persistor={persistor} loading={null}>
+      <App />
+    </PersistGate>
   </Provider>
 );
 
