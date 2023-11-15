74bec290f1323354bb9252f40e721d269a567c23
ZohaibA99 <118318498+ZohaibA99@users.noreply.github.com>
2023-11-14 21:31:48 -0500

    finished UI for profile page, protected route for profile page

--- api/models/user.model.js
+++ api/models/user.model.js
@@ -16,6 +16,11 @@ const userSchema = new mongoose.Schema({
     password: {
         type: String,
         required: true,
+    },
+    profilePicture: {
+        type: String,
+        default: "https://img.freepik.com/free-vector/mysterious-mafia-man-smoking-cigarette_52683-34828.jpg?size=626&ext=jpg",
+
     }
 }, {timestamps: true});
 
--- api/routes/auth.route.js
+++ api/routes/auth.route.js
@@ -1,9 +1,10 @@
 const express = require('express');
-const {signUp, signIn} = require('../controller/auth.controller.js');
+const {signUp, signIn, google} = require('../controller/auth.controller.js');
 
 const router = express.Router();
 
 router.post('/signup', signUp);
 router.post('/signin', signIn);
+router.post('/google', google);
 
 module.exports = router;
